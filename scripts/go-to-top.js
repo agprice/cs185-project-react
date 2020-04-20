4afd1626ea36605b6657929af6cde8c154a7329e
anonymous <anonymous>
2020-04-19 21:20:26 -0700

    ap - Adding some fancy external scripts to scroll to top

---
+++ scripts/go-to-top.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var topBtn = document.getElementById("topBtn");
+
+});
+window.onscroll = function() {
+    showTopBtn()
+};
+
+function showTopBtn() {
+    var documentHeight = $(document).height()
+    var scrollTop = $(window).scrollTop()
+    var windowHeight = $(window).height()
+    var scrollPercent = Math.round(scrollTop / (documentHeight - windowHeight) * 100)
+    console.log(scrollPercent);
+    if (scrollPercent > 25) {
+        $(topBtn).fadeIn(200);
+    } else {
+        $(topBtn).fadeOut(200);
+
+    }
+}
+
+// This takes the user to the top of the page
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python -m SimpleHTTPServer
+python3 -m http.server 
