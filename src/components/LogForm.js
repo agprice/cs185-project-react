de4111d6f4a9102b3df585540a4de8e61972b73e
anonymous <anonymous>
2020-05-08 22:23:33 -0700

    ap - Added some forms things placeholders and changed the default icon

--- src/components/LogForm.js
+++ src/components/LogForm.js
@@ -11,16 +11,16 @@ export default class LogForm extends Component {
             <form className='w3-container w3-padding w3-dark-gray w3-round-large' onSubmit={this.props.submitHandler}>
                 <div className="w3-panel">
                     <div className='w3-group w3-margin-bottom'>
-                        <label className='w3-label'>Name</label>
-                        <input  minLength="6" maxLength="19" className="w3-input" type="text" name="name" required />
+                        <label className='w3-label'>Name *</label>
+                        <input placeholder="Your name here" minLength="6" maxLength="19" className="w3-input" type="text" name="name" required />
                     </div>
                     <div className='w3-group w3-margin-bottom'>
                         <label className='w3-label'>Short Description of yourself</label>
-                        <input maxLength="99" className="w3-input" type="text" name="about" />
+                        <input placeholder="Tell us about yourself" maxLength="99" className="w3-input" type="text" name="about" />
                     </div>
                     <div className='w3-group w3-margin-bottom'>
-                        <label className='w3-label'>Message</label>
-                        <textarea minLength="16" maxLength="499" className="w3-input" type="text" style={{ height: '150px' }} name="message" required />
+                        <label className='w3-label'>Message *</label>
+                        <textarea placeholder="Your message" minLength="16" maxLength="499" className="w3-input" type="text" style={{ height: '150px' }} name="message" required />
                     </div>
 
                     <div>
@@ -30,7 +30,7 @@ export default class LogForm extends Component {
 
                     <div className='w3-group w3-margin-bottom'>
                         <label className='w3-label'>Email (Email will not be visible to site visitors)</label>
-                        <input className="w3-input" type="text" name="email" />
+                        <input placeholder="Your email here. It won't be shown to others." className="w3-input" type="text" name="email" />
                     </div>
                 </div>
                 <input type="submit" value="Post Message" />
