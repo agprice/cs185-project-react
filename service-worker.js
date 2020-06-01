c52d096bab1c9c1ccc35e8cbf62ea4f0f1618ee6
anonymous <anonymous>
2020-06-01 03:02:53 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185-project-react/precache-manifest.a315f2b1468af64ed0e33c08762a6a7a.js"
+  "/cs185-project-react/precache-manifest.efb1fff4e7a182aff6b8458f408cd4aa.js"
 );
 
 self.addEventListener('message', (event) => {
